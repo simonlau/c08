e11611eb0bd2c21c2b086295f683db3d7403f55a
Simon Lau <simon.lau.sg@gmail.com>
2026-07-15 16:39:18 +0800

    fix: add brackets to macros

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 22:54:21 by simon.lau         #+#    #+#             */
-/*   Updated: 2026/07/15 16:29:30 by simon.lau        ###   ########.fr       */
+/*   Updated: 2026/07/15 16:38:19 by simon.lau        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 
-# define EVEN(n) n % 2 == 0
+# define EVEN(n) (n % 2 == 0)
 
 typedef int	t_bool;
 
